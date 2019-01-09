37f2fb94812235202624e5277d92e4f495b908f1
Matthew Philps <philps.matthew@gmail.com>
2019-01-09 21:47:08 +0900

    test for isFull written for Stack

--- src/dataStruct.spec.js
+++ src/dataStruct.spec.js
@@ -1,6 +1,7 @@
 const Stack = require('./dataStruct')
 
 describe ('the Stack data structure',()=>{
+    // Set Up
     let stack = new Stack(3)
     it('should construct the stack with the defined capacity',()=>{
         expect(stack.items).toEqual([])
@@ -12,5 +13,15 @@ describe ('the Stack data structure',()=>{
     it('should have isEmpty return false if Stack not empty',()=>{
         stack.items.push(2)
         expect(stack.isEmpty()).toBe(false)
+        // Tear Down
+        stack = new Stack(3)
+    })
+    it('should have an isFull method returning true when Stack is at capacity',()=>{
+        for(let i =0; i<3; i++){
+            stack.items.push(i);
+        }
+        expect(stack.isFull()).toBe(true)
+        // Tear Down
+        stack = new Stack(3)
     })
 })
