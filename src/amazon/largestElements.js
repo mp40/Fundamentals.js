c8a227d1305b87c00d6d095441b0eb51f9080a2d
Matthew Philps <philps.matthew@gmail.com>
2019-01-27 15:05:02 +0900

    used modified bubble sort to find largest K numbers in array

--- src/amazon/largestElements.js
+++ src/amazon/largestElements.js
@@ -8,10 +8,26 @@ i.e., k = 3 then your program should print 50, 30 and 23.
 */
 
 const findLargest = function(arr, k){
-    let result = arr.sort(function(a,b){
-        return a - b 
-    })
-    return result.slice(arr.length-k)
+    //modified bubble sort
+    for(let i = 0; i<k; i++){
+        for(let j = 0; j<(arr.length-i-1); i++){
+            if(arr[j]>arr[j+1]){
+                const lesser = arr[j+1]
+                arr[j+1]=arr[j]
+                arr[j]= lesser
+            }
+        }
+    }
+    return arr.slice(k+1)
 }
 
+
 module.exports = findLargest
+
+// first attempt
+// const findLargest = function(arr, k){
+//     let result = arr.sort(function(a,b){
+//         return a - b 
+//     })
+//     return result.slice(arr.length-k)
+// }
