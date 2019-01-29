95e99e1d16e919ff6a87e2a834a7b46a3f896302
Matthew Philps <philps.matthew@gmail.com>
2019-01-30 01:06:11 +0900

    wip paths

---
+++ src/amazon/paths.js
@@ -0,0 +1,54 @@
+import { applyPatches } from "immer";
+
+/* 
+N x N matrix, start at 0,0, end at n,n
+find path to end 
+return path as array
+*/
+
+/*
+[
+    [o,o,o]
+    [o,o,o]
+    [o,o,o]
+]
+*/
+
+function paths(matrix){
+    const stack = [[0,0]]
+    matrix[0,0] = 'v'
+ 
+    while(stack.length>0){
+        let last = stack.length-1
+        let row = stack[last][0]
+        let col = stack[last][1]
+        
+        if(row === matrix.length-1 && col === matrix.length-1){
+            return stack
+        }
+        console.log(stack)
+        if(col+1 && matrix[row][col+1] === "o"){
+            col = col+1
+            matrix[row][col] = "v"
+            stack.push([row,col])
+        } else if (row+1 && matrix[row+1][col] === "o") {
+            row = row+1
+            matrix[row][col] = "v"
+            stack.push([row,col])
+        } else if (col-1 && matrix[row][col-1] === "o"){
+            col = col-1
+            matrix[row][col] = "v"
+            stack.push([row,col])
+        } else if (row -1 && matrix[row-1][col] === 'o'){
+            row = row-1
+            matrix[row][col] = "v"
+            stack.push([row,col])
+        } else {
+            console.log("POP")
+            stack.pop()
+        }
+    }
+    return false
+}
+
+module.exports = paths
---
+++ src/amazon/paths.spec.js
@@ -0,0 +1,30 @@
+const paths = require('./paths')
+
+describe('path finding',()=>{
+    const o = 'o'
+    const x = 'x'
+    const matrix = [
+        [o,o,o],
+        [o,o,o],
+        [o,o,o]
+    ]
+    // it('should return array of path to end',()=>{
+    //     expect(paths(matrix)[paths(matrix).length-1]).toEqual([2,2])
+    // })
+    // const blockedMatrix = [
+    //     [o,x,x],
+    //     [o,x,x],
+    //     [o,o,o]
+    // ]
+    // it('should find a path through a blocked matrix',()=>{
+    //     expect(paths(blockedMatrix)[paths(blockedMatrix).length-1]).toEqual([2,2])
+    // })
+    it('should be able to backtrack',()=>{
+        const backMatrix = [
+            [o,o,x],
+            [o,x,x],
+            [o,o,o]   
+        ]
+        expect(paths(backMatrix)[paths(backMatrix).length-1]).toEqual([2,2])
+    })
+})
