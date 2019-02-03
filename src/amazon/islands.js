7d34fba8ea0524c14fae649b5431cb1706a42902
Matthew Philps <philps.matthew@gmail.com>
2019-02-03 16:41:05 +0900

    islands completed

--- src/amazon/islands.js
+++ src/amazon/islands.js
@@ -3,14 +3,24 @@ function islands (matrix){
     const n = matrix.length
 
     function exploreIsland(row,col){
+        // mark island as 0
+        matrix[row][col] = 0
         // explore row + 1
-
+        if(row+1 < n && matrix[row+1][col]){
+            exploreIsland(row+1,col)
+        }
         // explore row - 1
-
+        if(row-1 >= 0 && matrix[row-1][col]){
+            exploreIsland(row-1, col)
+        }
         // explore col + 1
-
+        if(col+1 < n && matrix[row][col+1]){
+            exploreIsland(row, col+1)
+        }
         // explore col - 1
-
+        if(col-1 >= 0 && matrix[row][col-1]){
+            exploreIsland(row, col-1)
+        }
     }
     // check each element
     for(let i=0; i<n; i++){
